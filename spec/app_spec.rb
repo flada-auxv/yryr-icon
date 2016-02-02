d5224882482a18c3d39933ddbac1d90eadacc96f
flada-auxv <aseknock@gmail.com>
2016-02-03 00:07:03 +0900

    :feelsgood:

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe YRYRIcon do
-  describe 'when not logged in' do
+  describe 'when not signed in' do
     before do
       get '/'
     end
 
     it { expect(last_response).to be_redirect }
-    it { expect(last_response.location).to be_include('/login') }
+    it { expect(last_response.location).to be_include('/sign_in') }
   end
 end
