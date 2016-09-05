f3328742f2a8a197488311e92ddbebf518dfcbff
Koichi Tanaka <tanaka51.jp@gmail.com>
2016-09-06 06:56:41 +0900

    Continue updating even if it occurs the error

--- scripts/worker.rb
+++ scripts/worker.rb
@@ -3,4 +3,15 @@ Dotenv.load
 
 require_relative '../yryr_icon'
 
-User.includes(:schedule).where(schedules: {hours: Time.now.hour}).map(&:update_icon_randomly)
+User.includes(:schedule).where(schedules: {hours: Time.now.hour}).each do |user|
+  puts "start updating. user_id: #{user.id}"
+
+  begin
+    user.update_icon_randomly
+  rescue => e
+    # to continue updating, skip the error
+    puts "Scheduled update failed. user_id: #{user.id}"
+    puts e.class, e.message
+    puts e.backtrace.join("\n")
+  end
+end
